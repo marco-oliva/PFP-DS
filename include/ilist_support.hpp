cdb832b8c49bbbe933651b3c5839611b8804ca5e
Marco Oliva <marco.oliva@ufl.edu>
2022-10-11 10:33:34 -0400

    Working on example

---
+++ include/ilist_support.hpp
@@ -0,0 +1,84 @@
+/* pfp - prefix free parsing suffix array support
+Copyright (C) 2020 Ondřej Cvacho, nope
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see http://www.gnu.org/licenses/ .
+*/
+/*!
+\file ilist_support.hpp
+\brief ilist_support.hpp define and build the prefix-free parsing suffix array support data structures.
+\author Ondřej Cvacho, nope
+\date 03/04/2020
+*/
+
+
+#ifndef _PFP_ILIST_SUPPORT_HH
+#define _PFP_ILIST_SUPPORT_HH
+
+#include <utils.hpp>
+
+#include <sdsl/rmq_support.hpp>
+#include <sdsl/int_vector.hpp>
+
+#include <pfp.hpp>
+#include <wt.hpp>
+
+namespace pfpds
+{
+
+template<typename dict_data_type, class wt_t = pfp_wt_custom>
+class pfp_ilist_support {
+public:
+    pf_parsing<dict_data_type, wt_t>& pfp;
+
+    pfp_ilist_support(pf_parsing<dict_data_type, wt_t> & pfp_)
+        : pfp(pfp_)
+    { }
+
+    size_t size() const {
+        return pfp.n;
+    }
+
+    std::vector<std::size_t> operator()(std::size_t i) {
+        // i + 1 -> rank is for (0 ... i - 1) interval
+        const auto rank_i = pfp.b_bwt_rank_1(i + 1);
+        const auto lex_rank_i = rank_i - 1;
+        const auto interval_rank = i - pfp.b_bwt_select_1(rank_i); // lex rank - 0-based
+        const auto & m = pfp.M[lex_rank_i];
+    
+        // WT have 0 delimiter, lex smallest, so it shifted intervals
+        // rank + 1 because its 0-based and select is 1-based
+        const auto k = pfp.w_wt.range_select(m.left, m.right, interval_rank + 1);
+    
+        uint32_t p_i;
+        if (pfp.pars.saP[k + 1] > 0)
+            p_i = pfp.pars.saP[k + 1] - 1;
+        else
+            p_i = pfp.pars.p.size() - 2;
+    
+        size_t occ_k_next;
+        if (p_i + 2 > pfp.pars.p.size() - 1)
+            occ_k_next = pfp.n;
+        else
+            occ_k_next = pfp.select_b_p(p_i + 2); // start of next phrase in S
+    
+        if (occ_k_next < m.len)
+            return pfp.n - (m.len - occ_k_next);
+        else
+            return occ_k_next - m.len; // b_p starts with trigger string (occ_k_next - (m.len - pfp.w) - pfp.w)
+    }
+};
+
+}
+
+#endif /* end of include guard: _PFP_ILIST_SUPPORT_HH */
--- tests/unit_tests.cpp
+++ tests/unit_tests.cpp
@@ -155,6 +155,8 @@ void read_fasta_file(const char *filename, std::vector<char_type>& v){
 
 TEST_CASE( "pfp<uint8_t> from example", "PFP on example" )
 {
+    size_t w = 2;
+    
     std::vector<char> text = {'A','C','G','T','T','C','G','C','A','A','C','T','A','G','T','C','C','G','G','G','A','G','T','T','A','C','#',
                               'A','C','G','T','T','C','G','G','A','A','T','A','G','T','T','C','C','G','G','G','A','G','G','T','T','A','A','C','#',
                               'A','A','C','T','T','C','G','T','C','A','C','C','T','A','G','T','C','C','G','G','G','A','G','G','T','A','A','C','#',
@@ -164,183 +166,38 @@ TEST_CASE( "pfp<uint8_t> from example", "PFP on example" )
                               'A','C','G','T','T','C','G','T','G','A','C','T','A','G','T','T','C','C','G','G','G','A','C','T','A','A','C','#',
                               'A','C','C','T','T','C','G','C','A','C','C','T','A','G','T','C','C','G','G','G','A','G','T','T','A','C','#','#'
                               };
-    std::vector<std::string> dict{
-            "##AC", "AC##", "AC#AAC", "AC#AC", "ACCTAGT", "ACCTTCG", "ACG", "ACTAAC", "ACTAGT", "ACTTCG",
-            "CGCAAC", "CGCAC", "CGGAATAGT", "CGGGAC", "CGGGAGGT", "CGGGAGT", "CGT", "GTAAC", "GTCAC", "GTCCG",
-            "GTGAC", "GTTAAC", "GTTAC", "GTTCCG", "GTTCG", "GTTTAC",
+    std::vector<std::string> dict
+    {
+        "##AC", "AC##", "AC#AAC", "AC#AC", "ACCTAGT", "ACCTTCG", "ACG", "ACTAAC", "ACTAGT", "ACTTCG",
+        "CGCAAC", "CGCAC", "CGGAATAGT", "CGGGAC", "CGGGAGGT", "CGGGAGT", "CGT", "GTAAC", "GTCAC", "GTCCG",
+        "GTGAC", "GTTAAC", "GTTAC", "GTTCCG", "GTTCG", "GTTTAC",
     };
-    std::vector<uint32_t> parse{0, 6, 16, 24, 10, 8, 19, 15, 22, 3, 6, 16, 24, 12, 23, 14, 21, 2, 9, 16, 18, 4, 19,
-                                14, 17, 2, 9, 11, 4, 19, 13, 6, 16, 25, 3, 6, 16, 24, 16, 18, 8, 23, 15, 22, 2, 9,
-                                12, 19, 13, 7, 3, 6, 16, 24, 16, 20, 8, 23, 13, 7, 3, 5, 11, 4, 19, 15, 22, 1, 0};
-
-
-    std::vector<uint32_t> indices{0, 1, 2, 3, 4};
-    std::vector<uint8_t> dict2 = {'#', '#', 'G', 'A', 'T', 'T', 'A', 'C', EndOfWord,
-                                  'A', 'C', 'A', 'T', '#', EndOfWord,
-                                  'A', 'G', 'A', 'T', 'A', '#', '#', EndOfWord,
-                                  'T', '#', 'G', 'A', 'T', 'A', 'C', EndOfWord,
-                                  'T', '#', 'G', 'A', 'T', 'T', 'A', 'G', EndOfWord, EndOfDict};
-    sdsl::bit_vector b_d = {1, 0, 0, 0, 0, 0, 0, 0, 0,
-                            1, 0, 0, 0, 0, 0,
-                            1, 0, 0, 0, 0, 0, 0, 0,
-                            1, 0, 0, 0, 0, 0, 0, 0,
-                            1, 0, 0, 0, 0, 0, 0, 0, 0, 1};
-    const size_t n_phrases = 5;
-    const std::vector<size_t> phrase_length{8, 5, 7, 7, 8};
-    sdsl::bit_vector b_bwt = {1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1};
-
-    const std::vector<std::pair<size_t, std::pair<size_t, size_t>>> M{
-            {2, {0, 0}}, {6, {2, 2}}, {7, {3, 3}}, {7, {4, 4}}, {3, {0, 0}},
-            {2, {2, 3}}, {2, {4, 4}}, {3, {1, 1}}, {5, {0, 0}}, {4, {2, 2}},
-            {5, {3, 3}}, {5, {4, 4}}, {4, {1, 1}}, {6, {0, 0}}, {5, {2, 2}},
-            {6, {3, 3}}, {6, {4, 4}}, {2, {1, 1}}, {4, {0, 0}}, {3, {2, 3}},
-            {3, {4, 4}}, {4, {3, 3}}, {4, {4, 4}}
+    
+    std::vector<uint8_t> dict2;
+    for (auto& phrase : dict)
+    {
+        for (auto& c : phrase)
+        { dict2.push_back(c); }
+        dict2.push_back(EndOfWord);
+    }
+    dict2.push_back(EndOfDict);
+    
+    std::vector<uint32_t> parse
+    {
+        0, 6, 16, 24, 10, 8, 19, 15, 22, 3, 6, 16, 24, 12, 23, 14, 21, 2, 9, 16, 18, 4, 19,
+        14, 17, 2, 9, 11, 4, 19, 13, 6, 16, 25, 3, 6, 16, 24, 16, 18, 8, 23, 15, 22, 2, 9,
+        12, 19, 13, 7, 3, 6, 16, 24, 16, 20, 8, 23, 13, 7, 3, 5, 11, 4, 19, 15, 22, 1
     };
-    const std::vector<uint32_t> bwt_p = {3, 1, 4, 5, 2, 2};
-
-    // Extract the reverse of the phrases
-    std::vector<std::pair<std::string,uint32_t>> rev_dict;
-    int i = 0;
-    int rank = 0;
-    while(i < dict2.size()-1){
-    std::string s;
-    while(i < dict2.size()-1 && dict2[i] != EndOfWord) s.append(1,dict2[i++]);
-    i++;
-    reverse(s.begin(), s.end());
-    rev_dict.push_back({s,rank++});
-    }
-    std::sort(rev_dict.begin(),rev_dict.end());
-
-    std::vector<uint32_t> frequencies{0, 1, 2, 1, 1, 1};
-    size_t w = 2;
-
-    TEST_COUT << "Begin paper test" << std::endl;
-
-    pf_parsing<> pf(dict2, parse, frequencies, w);
-    pf_parsing<pfp_wt_custom> pf_c(dict2, parse, frequencies, w);
-    TEST_COUT << "Pfp built" << std::endl;
-
-    // TEST n
-    EXPECT_EQ(pf.n, text.size());
-    TEST_COUT << "Test n" << std::endl;
-
-    // TEST n_phrases
-    EXPECT_EQ(pf.dict.n_phrases(), n_phrases);
-    TEST_COUT << "Test n_phrases" << std::endl;
-
-    // TEST b_d
-    for(size_t i = 0; i < pf.dict.b_d.size(); ++i){
-    EXPECT_EQ(pf.dict.b_d[i], b_d[i]) << "at position: " << i;
-    }
-    TEST_COUT << "Test b_d" << std::endl;
-
-    // TEST phrase_length
-    for (size_t i = 0; i < phrase_length.size(); ++i)
-    {
-    EXPECT_EQ(pf.dict.length_of_phrase(i+1), phrase_length[i]);
-    }
-    TEST_COUT << "Test phrase_length" << std::endl;
-
-    // TEST b_bwt
-    for (size_t i = 0; i < pf.b_bwt.size(); ++i)
-    {
-    EXPECT_EQ(pf.b_bwt[i], b_bwt[i]) << "at position: " << i;
-    }
-    TEST_COUT << "Test b_bwt" << std::endl;
-
-    // TEST M
-    for (size_t i = 0; i < pf.M.size(); ++i)
-    {
-    EXPECT_EQ(pf.M[i].len, M[i].first) << "at position: " << i;
-    EXPECT_EQ(pf.M[i].left, M[i].second.first) << "at position: " << i;
-    EXPECT_EQ(pf.M[i].right, M[i].second.second) << "at position: " << i;
-    }
-    TEST_COUT << "Test M" << std::endl;
-
-    // TEST colex_document_array_helper
-    std::vector<uint_t> colex_id(pf.dict.n_phrases());
-    std::vector<uint_t> inv_colex_id(pf.dict.n_phrases()); // I am using it as starting positions
-    for (int i = 0, j = 0; i < pf.dict.d.size(); ++i)
-    if (pf.dict.d[i + 1] == EndOfWord)
-    {
-    colex_id[j] = j;
-    inv_colex_id[j++] = i;
-    }
-
-    pf.dict.colex_document_array_helper(inv_colex_id, colex_id, 0, pf.dict.n_phrases());
-    for (size_t i = 0; i < colex_id.size(); ++i){
-    EXPECT_EQ(colex_id[i], rev_dict[i].second) << "at position: " << i;
-    }
-    TEST_COUT << "Test colex_document_array_helper" << std::endl;
-
-    std::vector<uint32_t> sa(text.size(), 0);
-    std::iota(sa.begin(), sa.end(), 0);
-    auto cyclic_sort = [&](const size_t a, const size_t b) {
-        const auto max_cmp = text.size();
-        for (size_t i = 0; i < max_cmp; ++i) {
-            if (text[(a + i) % text.size()] != text[(b + i) % text.size()])
-                return text[(a + i) % text.size()] < text[(b + i) % text.size()];
-        }
-    };
-    std::sort(sa.begin(), sa.end(), cyclic_sort);
-
-    pfp_lce_support<> lce_ds(pf);
-    pfp_sa_support<> pf_sa(pf);
-
-    pfp_lce_support<pfp_wt_custom> lce_ds_sdsl(pf_c);
-    pfp_sa_support<pfp_wt_custom> pf_sa_sdsl(pf_c);
-
-    // TEST BWT(P) - wavelet tree
-    for (size_t i = 0; i < pf.w_wt.size(); ++i)
-    {
-    EXPECT_EQ(pf.w_wt[i], bwt_p[i]) << "at position: " << i;
-    }
-    TEST_COUT << "Test BWT(P)" << std::endl;
-
-    // TEST BWT(P) - wavelet tree
-    for (size_t i = 0; i < pf.w_wt.size(); ++i)
-    {
-    EXPECT_EQ(pf.w_wt[i], pf_c.w_wt[i]) << "at position: " << i;
-    }
-    TEST_COUT << "Test comparing WT custom vs. SDSL" << std::endl;
-
-    for (size_t i = 0; i < pf_sa.size(); ++i) {
-    EXPECT_EQ(pf_sa.sa(i), sa[i]) << "at position: " << i;
-    }
-    TEST_COUT << "Test PFP SA" << std::endl;
-
-    for (size_t i = 0; i < pf_sa.size(); ++i) {
-    EXPECT_EQ(pf_sa.sa(i), pf_sa_sdsl.sa(i)) << "at position: " << i;
-    }
-    TEST_COUT << "Test PFP comparing SA SDSL vs. custom" << std::endl;
-
-
-    uint8_t num_bytes = 1;
-    // build cst of the Text
-    TEST_COUT << "Computing CSA of the text" << std::endl;
-    std::vector<char> tmp_text = {'#', 'G', 'A', 'T', 'T', 'A', 'C', 'A', 'T', '#',
-                                  'G', 'A', 'T', 'A', 'C', 'A', 'T', '#',
-                                  'G', 'A', 'T', 'T', 'A', 'G', 'A', 'T', 'A', 0};
-    sdsl::csa_wt<> csa;
-    sdsl::construct_im(csa, static_cast<const char *>(&tmp_text[0]), num_bytes);
-    for (size_t i = 0; i < pf_sa.size(); ++i) {
-    EXPECT_EQ(pf_sa.sa(i), (csa[i] + (tmp_text.size()) - w + 1) % (tmp_text.size())) << "at position: " << i;
-    }
-    TEST_COUT << "Test PFP SA and cst" << std::endl;
-
-
-    std::vector<uint32_t> isa(text.size(), 0);
-    std::vector<uint32_t> lcp(text.size(), 0);
-    for (size_t i = 0; i < sa.size(); ++i)
-    isa[sa[i]] = i;
-
-    LCP_array_cyclic_text(&text[0], isa, sa, text.size(), lcp);
-
-    for (int i = 1; i < text.size() - 1; ++i)
-    {
-    EXPECT_EQ(lce_ds.lce(pf_sa.sa(i), pf_sa.sa(i + 1)), lcp[i + 1]) << "At positions: " << pf_sa.sa(i) << " " << pf_sa.sa(i + 1);
-    }
-    TEST_COUT << "Test LCE ds" << std::endl;
+    for (auto& p_id : parse) { p_id = p_id + 1; }
+    
+    std::vector<uint_t> frequencies(dict.size() + 1, 0);
+    for (auto& p_id : parse) { frequencies[p_id] += 1; }
+    
+    // build pfp
+    parse.push_back(0);
+    pfpds::pf_parsing<uint8_t> pfp(dict2, parse, frequencies, w);
+    
+    REQUIRE(true);
 }
 
 //------------------------------------------------------------------------------
